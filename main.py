0b2cfa25445231f812903c2cb98d2451335c4b1b
KruzhokProg <Aa1@3$5^7>
2022-03-22 14:33:26 +0300

    new

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import itertools
 
+
 # print(list(itertools.product('СОЛ', repeat=2)))
 # print(list(itertools.permutations('СОЛ')))
 def pol_3223():
@@ -10,3 +11,20 @@ def pol_3223():
             if 'ЕЙ' not in word and 'ЙЕ' not in word:
                 count += 1
     print(count)
+
+
+def pol_4199():
+    count = 0
+    glas = 'ЕИЯ'
+    sogl = 'КСН'
+    for r in range(3, 8):
+        for x in itertools.product('КСЕНИЯ', repeat=r):
+            word = ''.join(x)
+            if word.count('Е') <= 2 and word.count('И') <= 2 and word.count('Я') <= 2:
+                if ((word[0] in sogl) and len(set(word) & set(sogl)) == 1) or \
+                        len(set(word) & set(sogl)) == 0:
+                    count += 1
+    print(count)
+
+
+pol_4199()
